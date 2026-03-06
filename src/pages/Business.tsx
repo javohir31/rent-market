a0cb72036baf082ec0a7f0b88738e151653d6551
Javohir <narimonovjavohir2009@gmail.com>
2026-03-06 16:26:51 +0500

    first commit

--- src/pages/Business.tsx
+++ src/pages/Business.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import OrderTabs from '@/components/tabs/tabsOrder';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
 import AcountTabs from '@/components/tabs/tabsAcount';
