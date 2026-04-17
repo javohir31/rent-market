e105bbc7098b5c7b4df9468752ec44f090fa1d47
Otajon <atajanovatajan740@gmail.com>
2026-04-17 15:20:48 +0500

    bityotir

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,35 +1,12 @@
 import BannerRecom from "./BannerRecom"
 import BannerTwo from "./banner-two"
-import HowItWorks from "./HowItWorks"
-import Products from "./Products"
-import BannerInfo from "./BannerInfo"
-import express from "@/assets/Express.png"
-import bicycleGift from "@/assets/bicyclegift.png"
-import ProductsTwo from "./ProductsTwo"
-import LocationSection from "./LocationSection"
-import PartnersGrid from "./PartnersGrid"
-import FAQ from "./FAQ"
-import BlogSection from "./BlogSection"
-import { blogData } from "../data"
-
 
 const Banner = () => {
     return (
         <section>
             <div className="container">
                 <BannerRecom />
-                {/* <BannerTwo />
-                <Products title="Велосипеды" />
-                <BannerInfo img={express} />
-                <Products title="Спортивный тренажеры" />
-                <BannerInfo img={bicycleGift} reverse />
-                <ProductsTwo title="Гаджеты и другие" />
-                <BannerInfo img={express} />
-                <HowItWorks />
-                <LocationSection />
-                <PartnersGrid />
-                <FAQ />
-                <BlogSection data={blogData} /> */}
+                <BannerTwo />
             </div>
         </section>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,34 @@
 import Banner from "@/components/Banner"
+import BannerInfo from "@/components/BannerInfo"
+import Products from "@/components/Products"
+import express from "@/assets/Express.png"
+import bicycleGift from "@/assets/bicyclegift.png"
+import ProductsTwo from "@/components/ProductsTwo"
+import HowItWorks from "@/components/HowItWorks"
+import LocationSection from "@/components/LocationSection"
+import PartnersGrid from "@/components/PartnersGrid"
+import FAQ from "@/components/FAQ"
+import BlogSection from "@/components/BlogSection"
+import { blogData } from "@/data"
 
 
 
 const Home = () => {
   return (
-    <>
+    <div className="container">
       <Banner />
-    </>
+      <Products title="Велосипеды" />
+      <BannerInfo img={express} />
+      <Products title="Спортивный тренажеры" />
+      <BannerInfo img={bicycleGift} reverse />
+      <ProductsTwo title="Гаджеты и другие" />
+      <BannerInfo img={express} />
+      <HowItWorks />
+      <LocationSection />
+      <PartnersGrid />
+      <FAQ />
+      <BlogSection data={blogData} />
+    </div>
   )
 }
 
